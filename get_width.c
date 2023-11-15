cdc8fcc596c7dea1c6e73a5dfe1f5d97e13025f7
captynJoe <joelnjeng@gmail.com>
2023-11-14 18:39:05 -0800

    adfa

--- get_width.c
+++ get_width.c
@@ -1,28 +1,28 @@
 #include "main.h"
 
 /**
- * get_width - Calculates the width for printing
- * @format: Formatted string in which to print the arguments.
- * @i: List of arguments to be printed.
- * @list: list of arguments.
+ * get_width - Computes the width for printing
+ * @format: Formatted string with printing arguments.
+ * @a: List of arguments to be printed.
+ * @list: List of arguments.
  *
- * Return: width.
+ * Return: Width.
  */
-int get_width(const char *format, int *i, va_list list)
+int get_width(const char *format, int *a, va_list list)
 {
-	int curr_i;
+	int curr_a;
 	int width = 0;
 
-	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
+	for (curr_a = *a + 1; format[curr_a] != '\0'; curr_a++)
 	{
-		if (is_digit(format[curr_i]))
+		if (is_digit(format[curr_a]))
 		{
 			width *= 10;
-			width += format[curr_i] - '0';
+			width += format[curr_a] - '0';
 		}
-		else if (format[curr_i] == '*')
+		else if (format[curr_a] == '*')
 		{
-			curr_i++;
+			curr_a++;
 			width = va_arg(list, int);
 			break;
 		}
@@ -30,7 +30,7 @@ int get_width(const char *format, int *i, va_list list)
 			break;
 	}
 
-	*i = curr_i - 1;
+	*a = curr_a - 1;
 
 	return (width);
 }
